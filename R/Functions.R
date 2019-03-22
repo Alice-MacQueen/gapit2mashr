355d8dba6c8d9ac0870b2bd1bc9488e6c3982b5c
Alice-MacQueen <34892189+Alice-MacQueen@users.noreply.github.com>
2019-03-22 15:56:38 -0500

    Add non-exported functions to gapit2mashr.

--- R/Functions.R
+++ R/Functions.R
@@ -35,18 +35,20 @@ load_GAPIT_GWAS_all <- function(path = ".", phenotype, model = "CMLM"){
   # Probably want to test this to make sure path works with file.path and with
   # and without a trailing "/".
   out <- list()
-  out$Pred <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".", phenotype,
-                              ".PRED.csv")))
-  out$Results <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".", phenotype,
-                                 ".GWAS.Results.csv")), col_names = TRUE,
-                          col_types = "ciinninnnn")
-  out$Effects <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".", phenotype,
-                                 ".Df.tValue.StdErr.csv")), col_names = TRUE,
-                          col_types = "ciiinnn")
-  out$ROC <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".", phenotype,
-                                    ".ROC.csv")))
-  out$Log <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".", phenotype,
-                                    ".Log.csv")))
+  out$Pred <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".",
+                                                     phenotype, ".PRED.csv")))
+  out$Results <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".",
+                                                        phenotype,
+                                                        ".GWAS.Results.csv")),
+                                 col_names = TRUE, col_types = "ciinninnnn")
+  out$Effects <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".",
+                                                        phenotype,
+                                                      ".Df.tValue.StdErr.csv")),
+                                 col_names = TRUE, col_types = "ciiinnn")
+  out$ROC <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".",
+                                                    phenotype, ".ROC.csv")))
+  out$Log <- readr::read_csv(file.path(path, paste0("GAPIT.", model, ".",
+                                                    phenotype, ".Log.csv")))
   return(out)
 }
 
@@ -59,6 +61,50 @@ names(df2)[5] <- paste0(phenotype, "_tvalue")
 names(df2)[6] <- paste0(phenotype, "_stderror")
 names(df2)[7] <- paste0(phenotype, "_effect")
 return(df2)
+}
+
+s_hat_hedges_g <- function(df = df1$Results, phenotype){
+  standardization <- max(abs(df$effect), na.rm = TRUE)
+  df3 <- df %>%
+    mutate(Stand_effect = effect / standardization,
+           Obs = maf * nobs,
+           Obs2 = (1-maf) * nobs,
+           d = ifelse(abs(Stand_effect) < 0.98,
+                      (2 * Stand_effect) / sqrt(1 - Stand_effect^2),
+                      4),
+           d_unbiased = (1 - (3 / (4 * (nobs -2) -1))) * d,
+           sigma2_d = ((Obs + Obs2) / (Obs * Obs2)) +
+             (d_unbiased^2 / (2*(Obs + Obs2))),
+           stderr_d = sqrt(sigma2_d)) %>%
+    mutate(Stand_effect = ifelse(is.na(Stand_effect) |
+                                   is.infinite(Stand_effect),
+                                 0,
+                                 Stand_effect),
+           stderr_d = ifelse(is.na(stderr_d) | is.infinite(stderr_d),
+                             10,
+                             stderr_d))  %>%
+    dplyr::select(SNP, Stand_effect, stderr_d)
+  names(df3)[2] <- paste0("Bhat_", phenotype)
+  names(df3)[3] <- paste0("Shat_", phenotype)
+  return(df3)
+}
+
+s_hat_gapit <- function(df = df1$Effects, phenotype){
+  standardization <- max(abs(df$effect), na.rm = TRUE)
+
+  df3 <- df %>%  # fix this: make it a not-exported function.
+    mutate(stderr_d = `std Error` / standardization,
+           Stand_effect = effect / standardization) %>%
+    dplyr::select(SNP, Stand_effect, stderr_d) %>%
+    mutate(stderr_d = ifelse(is.na(stderr_d),
+                             10,
+                             stderr_d),
+           Stand_effect = ifelse(is.na(Stand_effect),
+                                 0,
+                                 Stand_effect))
+  names(df3)[2] <- paste0("Bhat_", phenotype)
+  names(df3)[3] <- paste0("Shat_", phenotype)
+  return(df3)
 }
 
 #' Convert GAPIT output to mashr input dataframes.
@@ -93,9 +139,9 @@ return(df2)
 #' where r is the effect size, scaled between -1 and 1; n's are the sample sizes
 #' of the two experimental groups; N is the total sample size.
 gapit2mashr <- function(path = ".", phenotypes = NA, numSNPs = 1000,
-                        model = "CMLM", S_hat = c("Hedge's G", "ones"),
+                        model = "CMLM", S_hat = c("Hedges' G", "ones"),
                         saveoutput = FALSE){
-  match.arg(stderr, c("Hedge's G", "ones")) # Fix this or make sure it works...
+  match.arg(stderr, c("Hedges' G", "ones")) # Fix this or make sure it works...
   if(is.na(phenotypes)){
     phe_col <- gapit_phenotypes_in_folder(path = path)
   } else {
@@ -109,28 +155,16 @@ gapit2mashr <- function(path = ".", phenotypes = NA, numSNPs = 1000,
                  " in the top ", numSNPs, " SNPs for at least one phenotype."))
 
   df1 <- load_GAPIT_GWAS_all(path = path, phenotype = phe_col[1])
-  df2 <- df1$Effects %>%  # fix this: make it a not-exported function.
-    mutate(abs_tvalue = abs(`t Value`)) %>%
-    top_n(as.integer(numSNPs), abs_tvalue)
-  names(df2)[4] <- paste0(phe_col[1], "_DF")
-  names(df2)[5] <- paste0(phe_col[1], "_tvalue")
-  names(df2)[6] <- paste0(phe_col[1], "_stderror")
-  names(df2)[7] <- paste0(phe_col[1], "_effect")
+  df2 <- gapit_top_effects(phenotype = phe_col[1])
   big_effects_df <- df2 %>%
     dplyr::select(-abs_tvalue)
 
   for(i in seq_along(phe_col)[-1]){
     df1 <- load_GAPIT_GWAS_all(path = path, phenotype = phe_col[i])
-    df2 <- df1$Effects %>%
-      mutate(abs_tvalue = abs(`t Value`)) %>%
-      top_n(as.integer(numSNPs), abs_tvalue)
-    names(df2)[4] <- paste0(phe_col[i], "_DF")
-    names(df2)[5] <- paste0(phe_col[i], "_tvalue")
-    names(df2)[6] <- paste0(phe_col[i], "_stderror")
-    names(df2)[7] <- paste0(phe_col[i], "_effect")
+    df2 <- gapit_top_effects(phenotype = phe_col[i])
     big_effects_df <- df2 %>%
       dplyr::select(-abs_tvalue) %>%
-      full_join(big_effects_df)
+      dplyr::full_join(big_effects_df)
   }
 
   if(saveoutput == TRUE){
@@ -142,55 +176,21 @@ gapit2mashr <- function(path = ".", phenotypes = NA, numSNPs = 1000,
           " Two: Creating strong and random dataframes of B_hat and S_hat",
           " values for use in mashr."))
 
-  df1 <- load_GAPIT_GWAS_all(path = "./", phenotype = phe_col[1])
+  df1 <- load_GAPIT_GWAS_all(path = path, phenotype = phe_col[1])
 
-  if(S_hat == "Hedge's G"){ # fix this: need support for "ones"
+  if(S_hat == "Hedges' G"){ # fix this: need support for "ones"
     if(sum(is.na(df1$Effects$`std Error`)) > length(df1$Effects$`std Error`)*.05){
       # If there are too many NA's for standard errors, derive new standard errors
-      # using Hedge's G (which requires the MAF).
-      standardization <- max(abs(df1$Results$effect), na.rm = TRUE)
-
-      df3 <- df1$Results %>% # fix this: make it a not-exported function.
-        mutate(Stand_effect = effect / standardization,
-               Obs = maf * nobs,
-               Obs2 = (1-maf) * nobs,
-               d = ifelse(abs(Stand_effect) < 0.98,
-                          (2 * Stand_effect) / sqrt(1 - Stand_effect^2),
-                          4),
-               d_unbiased = (1 - (3 / (4 * (nobs -2) -1))) * d,
-               sigma2_d = ((Obs + Obs2) / (Obs * Obs2)) +
-                 (d_unbiased^2 / (2*(Obs + Obs2))),
-               stderr_d = sqrt(sigma2_d)) %>%
-        mutate(Stand_effect = ifelse(is.na(Stand_effect) |
-                                       is.infinite(Stand_effect),
-                                     0,
-                                     Stand_effect),
-               stderr_d = ifelse(is.na(stderr_d) | is.infinite(stderr_d),
-                                 10,
-                                 stderr_d))  %>%
-        dplyr::select(SNP, Stand_effect, stderr_d)
-     names(df3)[2] <- paste0("Bhat_", phe_col[1])
-      names(df3)[3] <- paste0("Shat_", phe_col[1])
+      # using Hedges' G (which requires the MAF).
+      df3 <- s_hat_hedges_g(phenotype = phe_col[1])
       message(paste0("Hedge's G standard errors were used for", phe_col[1]))
   } else {
-    # or if not many of the standard errors are NA's, just use them for Shats.
-    standardization <- max(abs(df1$Effects$effect), na.rm = TRUE)
-
-    df3 <- df1$Effects %>%  # fix this: make it a not-exported function.
-      mutate(stderr_d = `std Error` / standardization,
-             Stand_effect = effect / standardization) %>%
-      dplyr::select(SNP, Stand_effect, stderr_d) %>%
-      mutate(stderr_d = ifelse(is.na(stderr_d),
-                               10,
-                               stderr_d),
-             Stand_effect = ifelse(is.na(Stand_effect),
-                                   0,
-                                   Stand_effect))
-    names(df3)[2] <- paste0("Bhat_", phe_col[i])
-    names(df3)[3] <- paste0("Shat_", phe_col[i])
-    message(paste0("GAPIT standard errors were used for", phe_col[1]))
+      # or if not many of the standard errors are NA's, just use them for Shats.
+      df3 <- s_hat_gapit(phenotype = phe_col[1])
+      message(paste0("GAPIT standard errors were used for", phe_col[1]))
   }
 
+  # Start making data frames of strong and random B_hat and S_hat.
   bhat_df <- big_effects_df %>%
     dplyr::select(SNP) %>%
     left_join(df3) %>%
@@ -200,7 +200,7 @@ gapit2mashr <- function(path = ".", phenotypes = NA, numSNPs = 1000,
     left_join(df3) %>%
     dplyr::select(SNP, starts_with("Shat"))
 
-  set.seed(1234)
+  set.seed(1234) # Makes the random data frames reproducible.
   random_sample <- sample(1:nrow(df3), nrow(big_effects_df)) %>% sort()
   bhat_random <- df3[random_sample,] %>%
     dplyr::select(SNP, starts_with("Bhat"))
@@ -208,51 +208,17 @@ gapit2mashr <- function(path = ".", phenotypes = NA, numSNPs = 1000,
     dplyr::select(SNP, starts_with("Shat"))
 
   for(i in seq_along(phe_col)[-1]){
-    df1 <- load_GAPIT_GWAS_all(path = "./", phenotype = phe_col[i])
+
+    df1 <- load_GAPIT_GWAS_all(path = path, phenotype = phe_col[i])
 
     if(sum(is.na(df1$Effects$`std Error`)) > length(df1$Effects$`std Error`)*.05){
       # If there are too many NA's for standard errors, derive new standard
       # errors using Hedge's G (which requires the MAF).
-      standardization <- max(abs(df1$Results$effect), na.rm = TRUE)
-
-      df3 <- df1$Results %>%
-        mutate(Stand_effect = effect / standardization,
-               Obs = maf * nobs,
-               Obs2 = (1-maf) * nobs,
-               d = ifelse(abs(Stand_effect) < 0.98,
-                          (2 * Stand_effect) / sqrt(1 - Stand_effect^2),
-                          4),
-               d_unbiased = (1 - (3 / (4 * (nobs -2) -1))) * d,
-               sigma2_d = ((Obs + Obs2) / (Obs * Obs2)) +
-                 (d_unbiased^2 / (2*(Obs + Obs2))),
-               stderr_d = sqrt(sigma2_d)) %>%
-        mutate(Stand_effect =
-                 ifelse(is.na(Stand_effect) | is.infinite(Stand_effect),
-                        0,
-                        Stand_effect),
-               stderr_d = ifelse(is.na(stderr_d) | is.infinite(stderr_d),
-                                 10,
-                                 stderr_d)) %>%
-        dplyr::select(SNP, Stand_effect, stderr_d)
-      names(df3)[2] <- paste0("Bhat_", phe_col[i])
-      names(df3)[3] <- paste0("Shat_", phe_col[i])
+      df3 <- s_hat_hedges_g(phenotype = phe_col[i])
       message(paste0("Hedge's G standard errors were used for ", phe_col[i]))
     } else {
       # or if not many of the standard errors are NA's, just use them for Shats.
-      standardization <- max(abs(df1$Effects$effect), na.rm = TRUE)
-
-      df3 <- df1$Effects %>%
-        mutate(stderr_d = `std Error` / standardization,
-               Stand_effect = effect / standardization) %>%
-        dplyr::select(SNP, Stand_effect, stderr_d) %>%
-        mutate(stderr_d = ifelse(is.na(stderr_d),
-                                 10,
-                                 stderr_d),
-               Stand_effect = ifelse(is.na(Stand_effect),
-                                     0,
-                                     Stand_effect))
-      names(df3)[2] <- paste0("Bhat_", phe_col[i])
-      names(df3)[3] <- paste0("Shat_", phe_col[i])
+      df3 <- s_hat_gapit(phenotype = phe_col[i])
       message(paste0("GAPIT standard errors were used for ", phe_col[i]))
     }
 
@@ -262,30 +228,29 @@ gapit2mashr <- function(path = ".", phenotypes = NA, numSNPs = 1000,
     shat_df <- shat_df %>%
       left_join(df3) %>%
       dplyr::select(SNP, starts_with("Shat"))
-
     bhat_random <- bhat_random %>%
       left_join(df3[random_sample,]) %>%
       dplyr::select(SNP, starts_with("Bhat"))
     shat_random <- shat_random %>%
       left_join(df3[random_sample,]) %>%
       dplyr::select(SNP, starts_with("Shat"))
-
-    B_hat_random <- column_to_rownames(bhat_random, "SNP")
-    S_hat_random <- column_to_rownames(shat_random, "SNP")
-    B_hat_strong <- column_to_rownames(bhat_df, "SNP")
-    S_hat_strong <- column_to_rownames(shat_df, "SNP")
+    }
   }
-}
+
+  B_hat_random <- column_to_rownames(bhat_random, "SNP")
+  S_hat_random <- column_to_rownames(shat_random, "SNP")
+  B_hat_strong <- column_to_rownames(bhat_df, "SNP")
+  S_hat_strong <- column_to_rownames(shat_df, "SNP")
 
   if(saveoutput == TRUE){
-  saveRDS(B_hat_strong, file = file.path(path, paste0("B_hat_strong_df_", numSNPs,
-                                                 "topSNPs.rds")))
-  saveRDS(S_hat_strong, file = file.path(path, paste0("S_hat_strong_df_", numSNPs,
-                                 "topSNPs.rds")))
-  saveRDS(B_hat_random, file = file.path(path, paste0("B_hat_random_df_", numSNPs,
-                                     "topSNPs.rds")))
-  saveRDS(S_hat_random, file = file.path(path, paste0("S_hat_random_df_", numSNPs,
-                                     "topSNPs.rds")))
+    saveRDS(B_hat_strong, file = file.path(path, paste0("B_hat_strong_df_",
+                                                        numSNPs, "topSNPs.rds")))
+    saveRDS(S_hat_strong, file = file.path(path, paste0("S_hat_strong_df_",
+                                                        numSNPs, "topSNPs.rds")))
+    saveRDS(B_hat_random, file = file.path(path, paste0("B_hat_random_df_",
+                                                        numSNPs, "topSNPs.rds")))
+    saveRDS(S_hat_random, file = file.path(path, paste0("S_hat_random_df_",
+                                                        numSNPs, "topSNPs.rds")))
   }
   return(list(SNP_df = big_effects_df, B_hat_strong = B_hat_strong,
               S_hat_strong = S_hat_strong, B_hat_random = B_hat_random,
